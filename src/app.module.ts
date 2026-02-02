6575ad9852a6adee7ce848028ba1dbf73eb3234d
Ivan <ivanmolina007@gmail.com>
2026-02-02 17:52:53 -0600

    before homework

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path/win32';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+
+    MongooseModule.forRoot('mongodb://localhost:27018/nest-pokemon'),
+
+    PokemonModule,
+
+    CommonModule,
+
+    SeedModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
