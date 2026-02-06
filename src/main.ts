53b03c0185a99d3db5aede1623f3c65d1f687440
Ivan <ivanmolina007@gmail.com>
2026-02-05 18:40:57 -0600

    improvements to deploy

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,8 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
-  console.log(`Application running on port ${process.env.PORT ?? 3000}`);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Application running on port ${port}`);
 }
 bootstrap();
