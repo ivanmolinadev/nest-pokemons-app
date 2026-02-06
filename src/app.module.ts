ba5df397f244a37635205b8ea50d6eb54001bec5
Ivan <ivanmolina007@gmail.com>
2026-02-05 18:51:22 -0600

    update path

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path/win32';
+import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
